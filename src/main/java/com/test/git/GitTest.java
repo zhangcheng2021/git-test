7968ad83ca08d705365d68b04be0259678ef1fa5
zhc <258477919@qq.com>
2021-08-25 14:43:32 +0800

    third control version

--- src/main/java/com/test/git/GitTest.java
+++ src/main/java/com/test/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git2");
+        System.out.println("git3");
     }
 }
