39e8a4e5f2e6e79a95c4fea78cc6c0ca4be2e78e
zhc <258477919@qq.com>
2021-08-25 15:00:28 +0800

    hot-fix control version

--- src/main/java/com/test/git/GitTest.java
+++ src/main/java/com/test/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git2");
+        System.out.println("git3");
+        System.out.println("git4");
     }
 }
